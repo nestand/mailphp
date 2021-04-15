6d2dec4b7464e39f683092dbf7c10d8b119932c3
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-04-15 21:44:06 +0200

    CSS added

--- index.php
+++ index.php
@@ -3,20 +3,20 @@
 	<head>
 		<title>Envoie un mail</title>
 		<meta charset="UTF-8">
-		<!-- <link rel="stylesheet" href="style.css"> -->
+		<link rel="stylesheet" href="main.css">
 	</head>
 	<body>
 	<?php
-		require ('send.php');
-		sendMail($_POST);
-	?>
+        require('send.php');
+        sendMail($_POST);
+    ?>
 	
 		<h1>Mail send</h1>
 		<form method="post" action="">
 			<label for="lastName">Nom : </label>
 			<input type="text" id="lastName" name="nom" required /><br/>
 			<label for="firstName">Votre message : </label>
-			<input type="text" id="message" name="message" required /><br/>
+			<input type="textarea" id="message" name="message" required /><br/>
 			<button type="submit">Valider</button>
 	
 		</form>
